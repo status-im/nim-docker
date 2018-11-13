3fd474103a6f9d02f2d0f0ac016a5171f6282564
Jacek Sieka <jacek@status.im>
2018-11-13 12:39:20 -0600

    docker: test

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
 
 set -ev
 
 buildAndPush() {
     cp bootstrap.sh $1
     docker build -t statusteam/$1 $1
-    docker push statusteam/$1
+#    docker push statusteam/$1
     rm $1/bootstrap.sh
 }
 
@@ -13,6 +14,6 @@ then
     buildAndPush debian-pre-nim
 fi
 
-buildAndPush nim-base
+#buildAndPush nim-base
 buildAndPush nim-base-arm32v7
-buildAndPush nim-base-arm64v8
+#buildAndPush nim-base-arm64v8
