c63b2da372855113432052f5066f1812c9e319e0
Jacek Sieka <jacek@status.im>
2018-12-10 09:38:54 -0600

    Revert "try lite build"
    This reverts commit 369bb9a44245494996539113fb38c9e654ebb7e3.

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ rm -rf /usr/share/man
 rm -rf /usr/share/locale
 wget https://github.com/facebook/rocksdb/archive/v5.14.2.tar.gz
 tar xvf v5.14.2.tar.gz
-cd rocksdb-5.14.2 && make LITE=1 install-shared -j3 && cd ..
+cd rocksdb-5.14.2 && make install-shared -j4 && cd ..
 rm -rf rocksdb-5.14.2 v5.14.2.tar.gz
 git clone https://github.com/status-im/nim.git
 cd nim
